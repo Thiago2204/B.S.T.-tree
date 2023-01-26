33169ebafdc1ce72dd57b4832944079442b51019
Thiago Henrique Quadrado Estacio <quadrado2204@gmail.com>
2023-01-26 10:00:19 -0300

    Create Utils.h

---
+++ Utils.h
@@ -0,0 +1,18 @@
+//Arthur Cardani Saraiva	32011334
+//Gabriel Woo Hyun Kim	32009968
+//Lucas Ellery Viana	42005541
+//Thiago Henrique Quadrado Estacio	42012740
+
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+namespace Utils
+{
+	template <typename T>
+	static T Max(T l, T r)
+	{
+		return (l > r) ? l : r;
+	}
+}
+
+#endif
